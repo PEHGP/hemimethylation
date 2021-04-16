02e0cafb1484a1fc0e5d446dad4a30e9cd054885
PEHGP <396777306@qq.com>
2021-04-16 14:15:01 +0800

    diff>=0.75 readscounts>=4

--- FindHemi.py
+++ FindHemi.py
@@ -25,13 +25,15 @@ def Gmt(MList):
 	p=res.pvalues[0]
 	#print(p)
 	return p
-def GetCGHemi(f,d):
+def GetCGHemi(f,d,dr):
 	lines=os.popen("zgrep -P '\tCG\t' %s"%f).readlines()
 	for i,x in enumerate(lines):
 		x=x.rstrip()
 		l=x.split("\t")
 		if not l[0] in d:
 			d[l[0]]={}
+		if not l[0] in dr:
+			dr[l[0]]={}
 		if l[2]=="-":
 			continue
 		rev=lines[i+1].rstrip().split("\t")
@@ -49,28 +51,35 @@ def GetCGHemi(f,d):
 				d[l[0]][l[1]].append(fwd_m/(fwd_m+fwd_um))
 			else:
 				d[l[0]][l[1]].append(0)
+			dr[l[0]][l[1]].append(fwd_m+fwd_um)
 			if rev_m+rev_um!=0:
 				d[l[0]][l[1]].append(rev_m/(rev_m+rev_um))
 			else:
 				d[l[0]][l[1]].append(0)
+			dr[l[0]][l[1]].append(rev_m+rev_um)
 		except:
 			d[l[0]][l[1]]=[]
+			dr[l[0]][l[1]]=[]
 			if fwd_m+fwd_um!=0:
 				d[l[0]][l[1]].append(fwd_m/(fwd_m+fwd_um))
 			else:
 				d[l[0]][l[1]].append(0)
+			dr[l[0]][l[1]].append(fwd_m+fwd_um)
 			if rev_m+rev_um!=0:
 				d[l[0]][l[1]].append(rev_m/(rev_m+rev_um))
 			else:
 				d[l[0]][l[1]].append(0)
-	return d
-def GetCHGHemi(f,d):
+			dr[l[0]][l[1]].append(rev_m+rev_um)
+	return d,dr
+def GetCHGHemi(f,d,dr):
 	lines=os.popen("zgrep -P '\tCHG\t' %s"%f).readlines()
 	for i,x in enumerate(lines):
 		x=x.rstrip()
 		l=x.split("\t")
 		if not l[0] in d:
 			d[l[0]]={}
+		if not l[0] in dr:
+			dr[l[0]]={}
 		if l[2]=="-":
 			continue
 		if l[-1]=="CCG":
@@ -90,33 +99,41 @@ def GetCHGHemi(f,d):
 				d[l[0]][l[1]].append(fwd_m/(fwd_m+fwd_um))
 			else:
 				d[l[0]][l[1]].append(0)
+			dr[l[0]][l[1]].append(fwd_m+fwd_um)
 			if rev_m+rev_um!=0:
 				d[l[0]][l[1]].append(rev_m/(rev_m+rev_um))
 			else:
 				d[l[0]][l[1]].append(0)
+			dr[l[0]][l[1]].append(rev_m+rev_um)
 		except:
 			d[l[0]][l[1]]=[]
+			dr[l[0]][l[1]]=[]
 			if fwd_m+fwd_um!=0:
 				d[l[0]][l[1]].append(fwd_m/(fwd_m+fwd_um))
 			else:
 				d[l[0]][l[1]].append(0)
+			dr[l[0]][l[1]].append(fwd_m+fwd_um)
 			if rev_m+rev_um!=0:
 				d[l[0]][l[1]].append(rev_m/(rev_m+rev_um))
 			else:
 				d[l[0]][l[1]].append(0)
-	return d
+			dr[l[0]][l[1]].append(rev_m+rev_um)
+	return d,dr
 if __name__ == '__main__':
 	prefix=sys.argv[1]
 	pvalues=float(sys.argv[2])
 	flist=sys.argv[3:] #CX_report.txt.gz list
 	diff=0.75 #need change
+	cutoff=4 #need change
 	Fr_CG=open(prefix+"_CG_hemi_ori.bed","w")
 	Fr_CHG=open(prefix+"_CHG_hemi_ori.bed","w")
 	dCHG={}
+	drCHG={}
 	dCG={}
+	drCG={}
 	for f in flist:
-		dCG=GetCGHemi(f,dCG)
-		dCHG=GetCHGHemi(f,dCHG)
+		dCG,drCG=GetCGHemi(f,dCG,drCG)
+		dCHG,drCHG=GetCHGHemi(f,dCHG,drCHG)
 	for c in dCG:
 		for p in dCG[c]:
 			MList=dCG[c][p]
@@ -125,17 +142,23 @@ if __name__ == '__main__':
 			rev=np.mean([MList[1],MList[3]])
 			p_value=Gmt(MList)
 			#print(c,p,dCG[c][p],p_value)
+			ReadsL=drCG[c][p]
+			fwdRs=(ReadsL[0]+ReadsL[2])/2.
+			revRs=(ReadsL[1]+ReadsL[3])/2.
 			if p_value<=pvalues:
-				Fr_CG.write(c+"\t"+p+"\t"+str(int(p)+1)+"\t"+str(fwd)+"\t"+str(rev)+"\n")
+				Fr_CG.write(c+"\t"+p+"\t"+str(int(p)+1)+"\t"+str(fwd)+"\t"+str(rev)+"\t"+str(fwdRs)+"\t"+str(revRs)+"\n")
 	for c in dCHG:
 		for p in dCHG[c]:
 			MList=dCHG[c][p]
 			fwd=np.mean([MList[0],MList[2]])
 			rev=np.mean([MList[1],MList[3]])
 			p_value=Gmt(MList)
+			ReadsL=drCHG[c][p]
+			fwdRs=(ReadsL[0]+ReadsL[2])/2.
+			revRs=(ReadsL[1]+ReadsL[3])/2.
 			if p_value<=pvalues:
-				Fr_CHG.write(c+"\t"+p+"\t"+str(int(p)+2)+"\t"+str(fwd)+"\t"+str(rev)+"\n")
+				Fr_CHG.write(c+"\t"+p+"\t"+str(int(p)+2)+"\t"+str(fwd)+"\t"+str(rev)+"\t"+str(fwdRs)+"\t"+str(revRs)+"\n")
 	Fr_CG.close()
 	Fr_CHG.close()
-	os.system("awk '$4-$5>=%s||$4-$5<=%s{print $1\"\t\"$2\"\t\"$3}' %s_CG_hemi_ori.bed|bedtools sort -i -|bedtools merge -d 5 -i - >%s_CG_hemi_final.bed"%(diff,-1*diff,prefix,prefix)) #-d need change
-	os.system("awk '$4-$5>=%s||$4-$5<=%s{print $1\"\t\"$2\"\t\"$3}' %s_CHG_hemi_ori.bed|bedtools sort -i -|bedtools merge -d 5 -i - >%s_CHG_hemi_final.bed"%(diff,-1*diff,prefix,prefix)) #-d need change
+	os.system("awk '($4-$5>=%s||$4-$5<=%s)&&($6>=%s||$7>=%s){print $1\"\t\"$2\"\t\"$3}' %s_CG_hemi_ori.bed|bedtools sort -i -|bedtools merge -d 5 -i - >%s_CG_hemi_final.bed"%(diff,-1*diff,cutoff,cutoff,prefix,prefix)) #-d need change
+	os.system("awk '($4-$5>=%s||$4-$5<=%s)&&($6>=%s||$7>=%s){print $1\"\t\"$2\"\t\"$3}' %s_CHG_hemi_ori.bed|bedtools sort -i -|bedtools merge -d 5 -i - >%s_CHG_hemi_final.bed"%(diff,-1*diff,cutoff,cutoff,prefix,prefix)) #-d need change
